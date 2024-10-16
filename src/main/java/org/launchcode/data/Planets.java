e594868008cd1ffa96c028949b5bd3e3799d6b92
Owner <braydens.coding@gmail.com>
2024-10-15 21:44:34 -0500

    Error persisting

--- src/main/java/org/launchcode/data/Planets.java
+++ src/main/java/org/launchcode/data/Planets.java
@@ -1,22 +1,25 @@
 package org.launchcode.data;
 
 public enum Planets {
-    MERCURY("Mercury", 88),
-    VENUS("Venus", 225),
-    EARTH("Earth", 365),
-    MARS("Mars", 687),
-    JUPITER("Jupiter", 4333),
-    SATURN("Saturn", 10759),
-    URANUS("Uranus", 30687),
-    NEPTUNE("Neptune", 60200);
+    MERCURY("Mercury", 88, 57),
+    VENUS("Venus", 225, 108),
+    EARTH("Earth", 365, 149),
+    MARS("Mars", 687, 228),
+    JUPITER("Jupiter", 4333, 780),
+    SATURN("Saturn", 10759, 1437),
+    URANUS("Uranus", 30687, 2871),
+    NEPTUNE("Neptune", 60200, 4530);
 
     private final String name;
 
     private final int yearLength;
 
-    Planets(String name, int yearLength) {
+    private final int distanceFromSun;
+
+    Planets(String name, int yearLength, int distanceFromSun) {
         this.name = name;
         this.yearLength = yearLength;
+        this.distanceFromSun = distanceFromSun;
     }
 
     public String getName() {
